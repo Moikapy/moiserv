ddcf89cfa46a4d2767cb781beef90bf841dfe11e
Warren Gates <warrenckhan@gmail.com>
2019-05-23 15:55:26 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+dotenv.config();
+var app = require("express")();
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
+
+const port = process.env.PORT || 5000;
+
+io.on("connection", (socket) =>{
+	console.log("a user connected");
+	socket.on("chat message", (msg) => {
+		if(msg.length !== 0){
+			io.emit("chat message", msg);
+		}
+	});
+});
+
+http.listen(port, () =>{
+	console.log(`listening on *:${port}`);
+});
