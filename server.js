7d9445a24952d44db0a166734a74de9730ff08bc
Warren Gates <warrenckhan@gmail.com>
2019-05-23 16:02:48 -0400

    add .env file

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ var http = require("http").createServer(app);
 var io = require("socket.io")(http);
 
 const port = process.env.PORT || 5000;
-
+app.get("/", function(req, res) {
+	res.send("<h1>Hello world</h1>");
+});
 io.on("connection", (socket) =>{
 	console.log("a user connected");
 	socket.on("chat message", (msg) => {
